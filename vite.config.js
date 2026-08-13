674a217a2b1374b85ba1362e1fe998ec46910abb
Peres <peres@example.com>
2026-08-12 23:45:11 -0400

    Update base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Projeto-Eletrico/",
+  base: "/energy-calculator/",
   plugins: [react()],
   server: {
     port: 5173,
